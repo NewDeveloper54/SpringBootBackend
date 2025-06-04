156a7c5526a9771e7ae2a822741e8c41b3a6114d
Amar AMEDJKANE <amedjkane.amar02@gmail.com>
2025-06-04 16:51:02 +0200

    remove dependency

--- src/main/java/com/amar/SpringProject/SpringProjectApplication.java
+++ src/main/java/com/amar/SpringProject/SpringProjectApplication.java
@@ -1,6 +1,5 @@
 package com.amar.SpringProject;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,11 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringProjectApplication {
 
 	public static void main(String[] args) {
-		// Charger les variables du fichier .env
-		Dotenv dotenv = Dotenv.configure().load();
+		// La variable MONGODB_URI est lue directement depuis les variables d'environnement
+		String uri = System.getenv("MONGODB_URI");
 
-		// Injecter la variable MONGODB_URI dans les propriétés système
-		System.setProperty("MONGODB_URI", dotenv.get("MONGODB_URI"));
+		// Tu peux afficher ou logguer l'URI si besoin (à retirer en production)
+		System.out.println("MongoDB URI: " + uri);
 
 		SpringApplication.run(SpringProjectApplication.class, args);
 	}
